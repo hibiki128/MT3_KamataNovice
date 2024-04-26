09463c3d7632a3b54b4d8c19aa45a48e87fcd0b9
hagiwara hibiki <146906515+hibiki128@users.noreply.github.com>
2024-04-26 16:47:08 +0900

    課題途中

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -10,20 +10,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	Vector3 v1{1.2f, -3.9f, 2.5f};
-	Vector3 v2{2.8f, 0.4f, -1.3f};
-	Vector3 cross = Cross(v1, v2);
-	Vector3 rotate{};
-	Vector3 translate{};
-	Vector3 cameraPosition{0.0f, 0.0f, -10.0f};
-	const int kWindowWidth = 1280;
-	const int kWindowHeight = 720;
-	Vector3 kLocalVertices[3] = {
-	    {0.0f,  0.5f,  0.0f},
-        {0.5f,  -0.5f, 0.0f},
-        {-0.5f, -0.5f, 0.0f}
-    };
-
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
@@ -41,42 +27,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		if (keys[DIK_W]) {
-			translate.z += 0.05f;
-		}
-		if (keys[DIK_S]) {
-			translate.z -= 0.05f;
-		}
-		if (keys[DIK_A]) {
-			translate.x -= 0.05f;
-		}
-		if (keys[DIK_D]) {
-			translate.x += 0.05f;
-		}
-		rotate.y += 0.05f;
-
-		Matrix4x4 worldMatrix = MakeAffineMatrix({1.0f, 1.0f, 1.0f}, rotate, translate);
-		Matrix4x4 cameraMatrix = MakeAffineMatrix({1.0f, 1.0f, 1.0f}, {0.0f, 0.0f, 0.0f}, cameraPosition);
-		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
-		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
-		// WVPMatrixを作る
-		Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
-		// ViewportMatrixを作る
-		Matrix4x4 viewportMatrix = MakeViewPortMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
-		// Screen空間へと頂点を変換する
-		Vector3 screenVertices[3];
-		for (uint32_t i = 0; i < 3; ++i) {
-			// NDCまで変換、Transformを使うと同次座標系->デカルト座標系の処理が行われ、結果的にZDivideが行われることになる
-			Vector3 ndcVertex = Transform(kLocalVertices[i], worldViewProjectionMatrix);
-			// Viewport変換を行ってScreen空間へ
-			screenVertices[i] = Transform(ndcVertex, viewportMatrix);
-		}
-
-		Vector3 crossV1 = Subtract(screenVertices[1], screenVertices[0]);
-		Vector3 crossV2 = Subtract(screenVertices[2], screenVertices[1]);
-		Vector3 crossDt = Cross(crossV1, crossV2);
-		float dot = Dot(cameraPosition, crossDt);
-
 		///
 		/// ↑更新処理ここまで
 		///
@@ -84,13 +34,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-
-		VectorScreenPrintf(0, 0, cross, "Cross");
-		if (dot <= 0) {
-			Novice::DrawTriangle(
-			    int(screenVertices[0].x), int(screenVertices[0].y), int(screenVertices[1].x), int(screenVertices[1].y),
-				int(screenVertices[2].x), int(screenVertices[2].y), RED, kFillModeSolid);
-		}
 
 		///
 		/// ↑描画処理ここまで
