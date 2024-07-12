6a0f7e50450a2678a98a9fc1c736a5c7c29dba19
hagiwara hibiki <146906515+hibiki128@users.noreply.github.com>
2024-07-12 11:00:33 +0900

    途中

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -5,8 +5,43 @@
 #include "algorithm"
 #include "imgui.h"
 #include <Novice.h>
+#define DELTA_TIME (1.0f / 60.0f)
 
 const char kWindowTitle[] = "LE2B_20_ハギワラ_ヒビキ";
+
+struct Spring {
+	Vector3 anchor;
+	float naturalLength;
+	float stiffness;
+	float dampingCoefficient;
+};
+
+struct Ball {
+	Vector3 position;
+	Vector3 velocity;
+	Vector3 acceleration;
+	float mass;
+	float radius;
+	unsigned int color;
+};
+
+void SpringMove(Spring& spring, Ball& ball) {
+	Vector3 diff = ball.position - spring.anchor;
+	float length = Length(diff);
+	if (length != 0.0f) {
+		Vector3 direction = Normalize(diff);
+		Vector3 restPosition = spring.anchor + direction * spring.naturalLength;
+		Vector3 displacement = length * (ball.position - restPosition);
+		Vector3 restoringForce = -spring.stiffness * displacement;
+		Vector3 dampringForce = -spring.dampingCoefficient * ball.velocity;
+		Vector3 force = restoringForce + dampringForce;
+		ball.acceleration = force / ball.mass;
+	}
+	// 加速度も速度もどちらも秒を基準にした値である
+	// それが、1/60秒(deltaTime)適用されたと考える
+	ball.velocity += ball.acceleration * DELTA_TIME;
+	ball.position += ball.velocity * DELTA_TIME;
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -22,16 +57,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	const int kWindowHeight = 720;
 	Vector2Int ClickPosition = {};
 
-	Vector3 a{0.2f, 1.0f, 0.0f};
-	Vector3 b{2.4f, 3.1f, 1.2f};
-	Vector3 c = a + b;
-	Vector3 d = a - b;
-	Vector3 e = a * 2.4f;
-	Vector3 rotate{0.4f, 1.43f, -0.8f};
-	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
-	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
-	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-	Matrix4x4 rotateMatrix = rotateXMatrix * rotateYMatrix * rotateZMatrix;
+	Spring spring{};
+	spring.anchor = {0.0f, 0.0f, 0.0f};
+	spring.naturalLength = 1.0f;
+	spring.stiffness = 100.0f;
+	spring.dampingCoefficient = 2.0f;
+
+	Ball ball{};
+	ball.position = {1.2f, 0.0f, 0.0f};
+	ball.mass = 2.0f;
+	ball.radius = 0.05f;
+	ball.color = BLUE;
+
+	Sphere sphere;
+	sphere.center = ball.position;
+	sphere.radius = ball.radius;
+
+	bool isStart = false;
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -57,6 +99,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
 		Matrix4x4 viewportMatrix = MakeViewPortMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
 
+		CameraMove(cameraRotate, cameraTranslate, ClickPosition, keys, preKeys);
+
+		if (isStart) {
+			SpringMove(spring, ball);
+		};
+
+		sphere.center = ball.position;
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -66,18 +116,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		ImGui::Begin("Window");
-		ImGui::Text("c:%f, %f, %f", c.x, c.y, c.z);
-		ImGui::Text("d:%f, %f, %f", d.x, d.y, d.z);
-		ImGui::Text("e:%f, %f, %f", e.x, e.y, e.z);
-		ImGui::Text(
-		    "matrix:\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n",
-			rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2], rotateMatrix.m[0][3], 
-			rotateMatrix.m[1][0],rotateMatrix.m[1][1], rotateMatrix.m[1][2], rotateMatrix.m[1][3],
-			rotateMatrix.m[2][0], rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3], 
-			rotateMatrix.m[3][0],rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]);
+		if (ImGui::Button("Start")) {
+			isStart = !isStart;
+		}
+		ImGui::DragFloat3("sphere center", &sphere.center.x, 0.1f);
+		ImGui::Text("start %d", isStart);
 		ImGui::End();
 
 		DrawGrid(viewProjectionMatrix, viewportMatrix, 2.0f, 10);
+		DrawSphere(sphere, viewProjectionMatrix, viewportMatrix, ball.color);
+		
 
 		///
 		/// ↑描画処理ここまで
