a79ba4ac62198c9930684af1fcdb5fddefecce13
hagiwara hibiki <146906515+hibiki128@users.noreply.github.com>
2024-07-22 14:21:41 +0900

    応用課題

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -5,7 +5,6 @@
 #include "algorithm"
 #include "imgui.h"
 #include <Novice.h>
-#include <cmath>
 
 const char kWindowTitle[] = "LE2B_20_ハギワラ_ヒビキ";
 
@@ -15,47 +14,75 @@ struct Capsule {
 };
 
 Vector3 Reflect(const Vector3& input, const Vector3& normal) {
+	// i・n を計算
 	float dotProduct = Dot(input, normal);
+
+	// 2(i・n)n を計算
 	Vector3 projection = normal * (2.0f * dotProduct);
-	return input - projection;
+
+	// r = i - 2(i・n)n
+	Vector3 reflection = input - projection;
+
+	return reflection;
 }
 
-bool IsCollision(const Capsule& capsule, const Plane& plane) {
-	Vector3 end1 = capsule.segment.origin;
-	Vector3 end2 = capsule.segment.origin + capsule.segment.diff;
+void ReflectMove(Ball& ball, Plane& plane, Capsule& capsules, const float& e) {
+	capsules.radius = ball.radius;
+	capsules.segment.origin = ball.position;
 
-	float distance1 = Dot(end1, plane.normal) - plane.distance;
-	float distance2 = Dot(end2, plane.normal) - plane.distance;
-
-	return (std::abs(distance1) <= capsule.radius || std::abs(distance2) <= capsule.radius);
-}
-
-void ReflectMove(Ball& ball, Plane& plane, float e) {
 	ball.velocity += ball.acceleration * DELTA_TIME;
 	ball.position += ball.velocity * DELTA_TIME;
 
-	Capsule capsule;
-	capsule.segment.origin = ball.position;
-	capsule.segment.diff = ball.velocity * DELTA_TIME;
-	capsule.radius = ball.radius;
+	capsules.segment.diff = ball.position - capsules.segment.origin;
 
-	if (IsCollision(capsule, plane)) {
+	if (IsCollision(Sphere{ball.position, ball.radius}, plane)) {
 		Vector3 reflected = Reflect(ball.velocity, plane.normal);
-		Vector3 planeToBall = ball.position - plane.normal * (Dot(ball.position, plane.normal) - plane.distance);
+		Vector3 projectToNormal = Project(reflected, plane.normal);
+		Vector3 movingDirection = reflected - projectToNormal;
+		ball.velocity = projectToNormal * e + movingDirection;
 
-		if (Dot(ball.position - planeToBall, plane.normal) < 0) {
-			ball.position = planeToBall;
+		Capsule capsule;
+		capsule.radius = ball.radius;
+		capsule.segment.origin = ball.position;
+		capsule.segment.diff = plane.normal * 4.0f;
+
+		float dot = Dot(capsule.segment.diff, plane.normal);
+		float t = plane.distance - Dot(capsule.segment.origin, plane.normal);
+		if (dot != 0.0f) {
+			t = t / dot;
+			if (-0.5f <= t && t <= 1.0f) {
+				Vector3 Point = capsule.segment.diff * t;
+				Vector3 Length = plane.normal * capsule.radius;
+				Point += Length;
+				ball.position += Point;
+			}
 		}
+	} else if (IsCollision(capsules.segment, plane)) {
+		Plane newPlane;
+		newPlane.normal = plane.normal;
+		newPlane.distance = plane.distance + ball.radius;
 
-		ball.velocity = reflected * e;
+		float dot = Dot(capsules.segment.diff, newPlane.normal);
+		float t = newPlane.distance - Dot(capsules.segment.diff, newPlane.normal);
+		t = t / dot;
+
+		Vector3 aPoint = capsules.segment.origin + (capsules.segment.diff * t);
+		ball.position = aPoint;
+
+		Vector3 reflected = Reflect(ball.velocity, plane.normal);
+		Vector3 projectToNormal = Project(reflected, plane.normal);
+		Vector3 movingDirection = reflected - projectToNormal;
+		ball.velocity = projectToNormal * e + movingDirection;
 	}
 }
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
+	// カメラの初期設定
 	Vector3 cameraTranslate{0.0f, 1.9f, -6.49f};
 	Vector3 cameraRotate{0.26f, 0.0f, 0.0f};
 	Vector3 cameraPosition{0.0f, 1.0f, -5.0f};
@@ -66,27 +93,36 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	bool isStart = false;
 
 	Plane plane;
-	plane.normal = Normalize({-0.2f, 1.2f, -0.3f});
+	plane.normal = Normalize({-0.2f, 0.9f, -0.3f});
 	plane.distance = 0.0f;
 
 	Ball ball{};
-	ball.position = {0.8f, 1.2f, 0.3f};
+	ball.position = {0.8f, 100.2f, 0.3f};
 	ball.acceleration = {0.0f, -9.8f, 0.0f};
 	ball.mass = 2.0f;
 	ball.radius = 0.05f;
 	ball.color = WHITE;
 
-	float e = 0.7f;
+	Capsule capsules;
 
+	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
+	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
 		Novice::BeginFrame();
 
+		// キー入力を受け取る
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
 
+		///
+		/// ↓更新処理ここから
+		///
+
+		// カメラの計算
 		Matrix4x4 cameraMatrix = MakeAffineMatrix({1.0f, 1.0f, 1.0f}, cameraRotate, cameraPosition + cameraTranslate);
 		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
 		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
@@ -96,30 +132,46 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		CameraMove(cameraRotate, cameraTranslate, ClickPosition, keys, preKeys);
 
 		if (isStart) {
-			ReflectMove(ball, plane, e);
+			ReflectMove(ball, plane, capsules, 0.7f);
 		}
-		if (ball.position.y < -0.6f) {
+		if (ball.position.y < -3) {
 			ball.position = {0.8f, 1.2f, 0.3f};
 			ball.velocity = {0.0f, 0.0f, 0.0f};
 		}
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
 
 		ImGui::Begin("Window");
 		if (ImGui::Button("Start")) {
 			isStart = !isStart;
 		}
+		ImGui::Text("isStart : %d", isStart);
 		ImGui::End();
 
 		DrawGrid(viewProjectionMatrix, viewportMatrix, 2.0f, 10);
 		DrawPlane(plane, viewProjectionMatrix, viewportMatrix, WHITE);
 		DrawBall(ball, viewProjectionMatrix, viewportMatrix, ball.color);
 
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
 		Novice::EndFrame();
 
+		// ESCキーが押されたらループを抜ける
 		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
 			break;
 		}
 	}
 
+	// ライブラリの終了
 	Novice::Finalize();
 	return 0;
 }
